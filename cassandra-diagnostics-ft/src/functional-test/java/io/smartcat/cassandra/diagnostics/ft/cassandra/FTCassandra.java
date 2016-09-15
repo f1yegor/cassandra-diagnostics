8f79651e5fcc1456a15e10d9f4456572f995ed41
Nenad Bozic <nbozicns@gmail.com>
2016-09-15 17:24:31 +0200

    Cassandra reporter for diagnostics.

---
+++ cassandra-diagnostics-ft/src/functional-test/java/io/smartcat/cassandra/diagnostics/ft/cassandra/FTCassandra.java
@@ -0,0 +1,55 @@
+package io.smartcat.cassandra.diagnostics.ft.basic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.thrift.transport.TTransportException;
+import org.assertj.core.api.Assertions;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+
+import io.netty.util.internal.SystemPropertyUtil;
+
+public class FTCassandra {
+
+    private static Cluster cluster;
+    private static Session session;
+
+    @BeforeClass
+    public static void setUp() throws ConfigurationException, TTransportException, IOException, InterruptedException {
+        cluster = Cluster.builder()
+                .addContactPoint(SystemPropertyUtil.get("cassandra.host"))
+                .withPort(Integer.parseInt(SystemPropertyUtil.get("cassandra.port")))
+                .build();
+        session = cluster.connect();
+    }
+
+    @Test
+    public void test() throws Exception {
+        session.execute("CREATE KEYSPACE IF NOT EXISTS test_keyspace "
+                + "WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };");
+        session.execute("CREATE TABLE IF NOT EXISTS test_keyspace.test_table (uid uuid PRIMARY KEY);");
+        session.execute("SELECT * FROM test_keyspace.test_table");
+        cluster.close();
+        
+        ResultSet result = session.execute("SELECT * FROM cassandra_dianostics.measurements");
+        int measurementsCount = result.all().size();
+
+        Assertions.assertThat(measurementsCount).isEqualTo(2);
+    }
+}
---
+++ cassandra-diagnostics-reporter-cassandra/src/main/java/io/smartcat/cassandra/diagnostics/reporter/CassandraReporter.java
@@ -0,0 +1,127 @@
+package io.smartcat.cassandra.diagnostics.reporter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ConsistencyLevel;
+import com.datastax.driver.core.DataType;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SimpleStatement;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.schemabuilder.SchemaBuilder;
+
+import io.smartcat.cassandra.diagnostics.Measurement;
+
+/**
+ * A Cassandra based {@link Reporter} implementation. Query reports are sent to cassandra.
+ */
+public class CassandraReporter extends Reporter {
+
+    /**
+     * Class logger.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(CassandraReporter.class);
+
+    private static final String CONTACT_POINT_PROP = "contactPoint";
+
+    private static final String PORT_PROP = "port";
+
+    private static final String DEFAULT_PORT = "9042";
+
+    private static final String KEYSPACE_NAME_PROP = "keyspace";
+
+    private static final String DEFAULT_KEYSPACE_NAME = "cassandra_dianostics";
+
+    private static final String CONSISTENCY_LEVEL_PROP = "consistencyLevel";
+
+    private static final String DEFAULT_CONSISTENCY_LEVEL = "ONE";
+
+    private static final SimpleDateFormat SIMPLE_DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
+
+    private PreparedStatement insertMeasurementStatement;
+
+    private static Session session;
+
+    private ConsistencyLevel consistencyLevel;
+
+    /**
+     * Constructor.
+     *
+     * @param configuration Reporter configuration
+     */
+    public CassandraReporter(ReporterConfiguration configuration) {
+        super(configuration);
+
+        if (!configuration.options.containsKey(CONTACT_POINT_PROP)) {
+            logger.warn("Not properly configured. Missing cassandra contact point. Aborting initialization.");
+            return;
+        }
+
+        String contactPoint = configuration.options.get(CONTACT_POINT_PROP);
+        int port = Integer.parseInt(configuration.getDefaultOption(PORT_PROP, DEFAULT_PORT));
+        String keyspace = configuration.getDefaultOption(KEYSPACE_NAME_PROP, DEFAULT_KEYSPACE_NAME);
+        consistencyLevel = ConsistencyLevel
+                .valueOf(configuration.getDefaultOption(CONSISTENCY_LEVEL_PROP, DEFAULT_CONSISTENCY_LEVEL));
+
+        Cluster cluster = Cluster.builder().addContactPoints(contactPoint).withPort(port).build();
+        session = cluster.connect();
+
+        createKeyspaceIfNotExists(keyspace);
+        createMeasurementTableIfNotExists();
+        insertMeasurementStatement = session.prepare("INSERT INTO " + keyspace
+                + ".measurements (name, host, date, timestamp, value, fields, tags) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    }
+
+    private void createKeyspaceIfNotExists(final String keyspace) {
+        session.execute("CREATE KEYSPACE IF NOT EXISTS " + keyspace + " "
+                + "WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };");
+    }
+
+    private void createMeasurementTableIfNotExists() {
+        final String createMeasurementsByHostTable = SchemaBuilder
+                .createTable("measurements")
+                .addPartitionKey("name", DataType.text())
+                .addPartitionKey("host", DataType.text())
+                .addPartitionKey("date", DataType.text())
+                .addClusteringColumn("timestamp", DataType.timestamp())
+                .addColumn("value", DataType.cdouble())
+                .addColumn("fields", DataType.frozenMap(DataType.text(), DataType.text()))
+                .addColumn("tags", DataType.frozenMap(DataType.text(), DataType.text()))
+                .ifNotExists().withOptions()
+                .comment("Measurement by host table.")
+                .buildInternal();
+
+        final Statement statement = new SimpleStatement(createMeasurementsByHostTable);
+        statement.setConsistencyLevel(ConsistencyLevel.ALL);
+        session.execute(statement);
+    }
+
+    @Override
+    public void report(Measurement measurement) {
+        if (session == null) {
+            logger.warn("Cassandra session is not initialized");
+            return;
+        }
+
+        long timestampInMillis = measurement.timeUnit().toMillis(measurement.time());
+
+        Date date = new Date(timestampInMillis);
+        String datePortion = SIMPLE_DATE_FORMATTER.format(date);
+
+        logger.debug("Sending Query: {}", measurement.toString());
+        try {
+            session.executeAsync(
+                    insertMeasurementStatement.bind(measurement.name(), "host", datePortion, date, measurement.value(),
+                            measurement.fields(), measurement.tags()).setConsistencyLevel(consistencyLevel));
+        } catch (Exception e) {
+            logger.warn("Failed to send report to Cassandra", e);
+        }
+    }
+
+}
+
